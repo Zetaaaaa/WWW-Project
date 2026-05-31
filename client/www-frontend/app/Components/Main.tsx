9f89efe357a4dc67e0ffbdb8015c5775e6bd9cff
Adam Jabłoński <adam.jab2004@gmail.com>
2026-05-31 22:19:41 +0200

    added socketIO broadcast logic

---
+++ client/www-frontend/app/Components/Main.tsx
@@ -0,0 +1,201 @@
+'use client'
+// ES modules
+import { io, Socket } from "socket.io-client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+interface Lobby {
+    name: string;
+    variant: string;
+    count: number;
+    players: [];
+}
+
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+import { Field, FieldGroup } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+function Main() {
+
+    const [socket, setSocket] = useState<Socket | any>(null);
+
+    const [userName, setUsername] = useState<string>('user')
+    const [lobbyName, setLobbyName] = useState<string | null>("Lobbyy")
+    const [lobbyList, setLobbyList] = useState(null)
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        // Initialize the Socket.IO connection
+        const socket = io('ws://localhost:1145');
+
+        // Socket.IO uses .on() for event listeners
+        socket.on('connect', () => {
+            console.log('Connected to Socket.IO server');
+        });
+
+        socket.on('disconnect', () => {
+            console.log('Disconnected from Socket.IO server');
+        });
+
+        // Listen for custom events sent from your server
+        // Note: The server must be using socket.emit('event_name', data)
+        socket.on('LOBBY_CREATED', (data) => {
+            console.log("lobby is up!!!", data);
+        });
+
+        socket.on('LOBBY_LIST', (lobbyData) => {
+            // console.log("GOT NEW LOBBY LIST:", lobbyData);
+            setLobbyList(lobbyData);
+        });
+
+        socket.on('errorResponse', (content) => {
+            console.error("ERROR", content);
+        });
+
+        // Store in state so you can use it elsewhere in your component
+        setSocket(socket);
+
+        // Cleanup on unmount
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+    function createLobby(lobbyName: string) {
+        console.log("Attempting creation of a new lobby");
+
+        // Socket.IO automatically handles serialization
+        socket.emit("CREATE_LOBBY", {
+            lobbyName: lobbyName,
+            userName: userName
+        });
+    }
+
+    function joinLobby(lobbyName: string) {
+        console.log("Attempting to join a lobby");
+
+        // Send the object directly
+        socket.emit("JOIN_LOBBY", {
+            lobbyName: lobbyName,
+            userName: userName
+        });
+    }
+    return (
+        // grid-cols-2
+        <div className="w-full h-full grid grid-cols-1 gap-1">
+
+            <div className="light:text-black text-center dark:text-sky-100">
+                <p>Actions</p>
+                <div className="flex mt-3 flex-col items-center gap-5">
+                    {/* <Button onClick={() => test(ws)} variant={"outline"}>Test Request</Button> */}
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button variant="outline">Create Lobby</Button>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-sm">
+
+                                <DialogHeader>
+                                    <DialogTitle>Create Lobby</DialogTitle>
+                                    <DialogDescription>
+                                        Create a game lobby to play with your friends!
+                                    </DialogDescription>
+                                </DialogHeader>
+                                <FieldGroup>
+                                    <Field>
+                                        <Label htmlFor="username">Username</Label>
+                                        <Input id="username" name="username" placeholder="The biggest liar" defaultValue={"user"} onInput={(e) => { setUsername(e.currentTarget.value) }} />
+                                    </Field>
+                                    <Field>
+                                        <Label htmlFor="lobbyname">Lobby Name</Label>
+                                        <Input id="lobbyname" type="text" placeholder="Liar's table" defaultValue={"1"} maxLength={20} minLength={2} onInput={(e) => { setLobbyName(e.currentTarget.value) }} />
+                                    </Field>
+                                </FieldGroup>
+                                <DialogFooter>
+                                    <DialogClose asChild>
+                                        <Button variant="outline">Cancel</Button>
+                                    </DialogClose>
+                                    <Button onClick={() => createLobby(lobbyName)} type="submit">Create</Button>
+                                </DialogFooter>
+
+                        </DialogContent>
+                    </Dialog>
+                    <Button onClick={() => console.log("im a dud")} variant={"outline"}>Settings</Button>
+                </div>
+            </div>
+            {/* <div className="bg-blue-400 row-span-4">
+                <p>ResponseBox</p>
+            </div> */}
+            {/* <p className="text-black text-lg font-semibold">Table</p> */}
+            {/* albo to https://reactbits.dev/components/animated-list */}
+            <Table>
+                <TableCaption>Online lobbies: {lobbyList != null ? lobbyList.length : "00a"}</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>__</TableHead>
+                        <TableHead>Lobby Name</TableHead>
+                        <TableHead>Variant</TableHead>
+                        <TableHead>People Count</TableHead>
+                        <TableHead>Actions</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+
+                    {lobbyList != null &&
+                        lobbyList.map(([id, lobby]: [string, Lobby]) => {
+                            return (
+                                <TableRow key={id}>
+                                    <TableCell>{id+1}.</TableCell>
+                                    <TableCell>{lobby.name}</TableCell>
+                                    <TableCell>{lobby.variant}</TableCell>
+                                    <TableCell>1/{lobby.count}</TableCell>
+                                    <TableCell>
+                                        <Dialog>
+                                            <DialogTrigger asChild>
+                                                <Button>Join</Button>
+                                            </DialogTrigger>
+                                            <DialogContent className="sm:max-w-sm">
+                                                <DialogHeader>
+                                                    <DialogTitle>Join the game : {lobby.name}</DialogTitle>
+                                                    <DialogDescription>
+                                                        Type your username and join the game!
+                                                    </DialogDescription>
+                                                </DialogHeader>
+                                                <FieldGroup>
+                                                    <Field>
+                                                        <Label htmlFor="username">Username</Label>
+                                                        <Input id="username" name="username" placeholder="The biggest liar" defaultValue={"user"} onInput={(e) => { setUsername(e.currentTarget.value) }} />
+                                                    </Field>
+                                                </FieldGroup>
+                                                <DialogFooter>
+                                                    <DialogClose asChild>
+                                                        <Button variant="outline">Cancel</Button>
+                                                    </DialogClose>
+                                                    {/* SUBJECT TO CHANGE DUE TO THE NATURE OF NAMES  ->> CODE JHD12S */}
+                                                    <Button onClick={() => joinLobby(lobby.name)} type="submit">Join</Button>
+                                                </DialogFooter>
+                                            </DialogContent>
+                                        </Dialog>
+                                    </TableCell>
+                                </TableRow>
+                            )
+                        })
+                    }
+                </TableBody>
+            </Table>
+        </div >
+    )
+}
+
+export default Main
--- server/server.js
+++ server/server.js
@@ -1,43 +1,108 @@
-const { WebSocketServer } = require("ws");
+// const { WebSocketServer } = require("ws");
+import { Server } from "socket.io";
 
-const wss = new WebSocketServer({ port: 1145 });
+const Status = Object.freeze({
+  IN_PROGRESS: "IN_PROGRESS",
+  FAILED: "FAILED",
+  COMPLETED: "COMPLETED",
+});
 
-const PlayerMap = new Map();
-const LobbyMap = new Map();
+const Variant = Object.freeze({
+  NORMAL: "normal",
+  RANDOMIZER: "randomizer",
+});
 
-wss.on("connection", (ws) => {
-  console.log("New user connected");
+class Lobby {
+  constructor(name, variant, count, players) {
+    this.name = name;
+    this.variant = variant;
+    this.count = count;
+    this.players = [];
+  }
+}
 
-  ws.on("message", (message) => {
-    console.log("$$$$$$$$$$$$$$ { New Request recived } $$$$$$$$$$$$$$$$$$");
+let PlayerMap = new Map();
+let PlayerArr = [];
+let LobbyMap = new Map();
 
-    const { username, type, data, lobbyName } = JSON.parse(message);
 
-    console.log("Type: ", type, " Data: ", data);
+//sample lobbies
+const lobbyTest = new Lobby("test", Variant.NORMAL, 5);
+const lobbyTest2 = new Lobby("test2", Variant.RANDOMIZER, 6);
+LobbyMap.set(0, lobbyTest);
+LobbyMap.set(1, lobbyTest2);
 
-    switch (type) {
-      case "TEST":
-        ws.send(
-          JSON.stringify({
-            type: "TestResponse",
-            content: "i see your request!",
-          }),
-        );
-        break;
-      case "CREATE_LOBBY":
-        //createlobbylogic
-        console.log("CREATE_LOBBY");
-        console.log("User creating lobby:", username);
-        
-        console.log("Lobby Name:", lobbyName);
-        
+// const wss = new WebSocketServer({ port: 1145 });
 
-        break;
-      case "JOIN_LOBBY":
-        console.log("JOIN_LOBBY");
+const io = new Server({
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-        //joinlobby
-        break;
+io.listen(1145);
+
+// make all Socket instances disconnect
+// io.disconnectSockets();
+
+io.on("connection", (socket) => {
+  console.log("New user connected:", socket.id);
+
+  
+
+  PlayerArr.push(socket.id);
+  const lobbyArray = Array.from(LobbyMap.entries());
+  socket.emit("LOBBY_LIST", lobbyArray);
+
+  socket.on("CREATE_LOBBY", ({ userName, lobbyName }) => {
+    console.log("User creating lobby:", userName, "Name:", lobbyName);
+
+    const result = createLobby(lobbyName);
+
+    if (!result) {
+      socket.emit("LOBBY_FAILED", { status: Status.FAILED, data: "nic" });
+    } else {
+      socket.emit("LOBBY_CREATED", { status: Status.COMPLETED, data: "nic" });
+
+      // Notify EVERYONE of the updated lobby list
+      const updatedList = Array.from(LobbyMap.entries());
+      io.emit("LOBBY_LIST", updatedList);
     }
   });
+  
+
+  socket.on("JOIN_LOBBY", ({ userName, lobbyName }) => {
+    console.log("User joining lobby:", userName, lobbyName);
+    // Add your join logic here
+    socket.join()
+  });
+
+   socket.on("disconnect", (reason) => {
+    // console.log(`User ${socket.id} left: ${reason}`);
+    PlayerArr = PlayerArr.filter((user) => user != socket.id);
+    console.log(PlayerArr);
+    
+  });
+
+
+//   // Get the room object
+// const room = io.sockets.adapter.rooms.get('lobby_123');
+
+// // Check if the room exists and get the count
+// const count = room ? room.size : 0;
+
+// console.log(`There are ${count} users in the room.`);
+
+  function createLobby(lobbyName, player) {
+    let players = [];
+    players.push(player);
+    const lobby = new Lobby(lobbyName, Variant.NORMAL, 5, players);
+    PlayerMap.set(lobbyName, lobby);
+
+    LobbyMap.set(LobbyMap.size, lobby);
+    console.log(LobbyMap);
+    io.emit("LOBBY_LIST", lobbyArray);
+    return true;
+  }
 });
