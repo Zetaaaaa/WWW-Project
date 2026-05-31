c3d92310652cc47e41d6db5838acbbe4bb699963
Adam Jabłoński <adam.jab2004@gmail.com>
2026-05-31 19:40:48 +0200

    added functional components

--- client/www-frontend/app/layout.tsx
+++ client/www-frontend/app/layout.tsx
@@ -1,14 +1,19 @@
 import Navbar from "./Components/navbar"
 import '@/app/globals.css'
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+
 export default function MainLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
-            <body className="h-screen flex flex-col" >
-                <Navbar></Navbar>
+        <html lang="en" className={cn("font-sans", geist.variable)}>
+            <body className="h-screen dark flex flex-col" >
                 <main className="flex-1">{children}</main>
             </body>
         </html>
